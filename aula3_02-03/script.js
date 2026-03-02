b6a3396f79de6d0f6eb754c24652ef5cd876b206
isabellyBS <isabellyBSmcm@gmail.com>
2026-03-02 08:08:54 -0300

    Add files via upload

---
+++ aula3_02-03/script.js
@@ -0,0 +1,63 @@
+// exercício a
+var aA = 10;
+var aB = 20;
+
+console.log(aB);
+
+aB = 5;
+
+console.log(aA, aB);
+
+
+console.log(" ")
+// Exercício b
+
+var bA = 30;
+var bB = 20;
+var bC =  bA + bB;
+
+console.log(bC);
+bB = 10;
+
+console.log(bB, bC);
+
+bC = bA + bB;
+
+console.log(bA, bB, bC);
+
+
+console.log(" ")
+//Exercício c
+var cA=10;
+var cB= 20;
+var cC= cA;
+cB = cC;
+cA = cB;
+
+console.log(cA, cB, cC);
+
+
+console.log(" ")
+//Exercício d
+var dA = 10;
+var dB = dA + 1;
+dA = dB + 1;
+dB = dA + 1; 
+
+console.log(dA);
+
+dA = dB + 1;
+
+console.log(dA, dB);
+
+
+console.log(" ")
+//Exercício 2
+var n1 = 2;
+var n2 = 3;
+var n3 = 5;
+
+var mediafinal= (n1*2+n2*3+n3*5) / 10;
+
+console.log ("Média Final!!!");
+console.log(mediafinal);
