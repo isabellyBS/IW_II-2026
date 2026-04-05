2df457ef36db560fef5e8bc8488583e716afad45
isabellyBS <isabellyBSmcm@gmail.com>
2026-04-04 23:26:09 -0300

    Add files via upload
    ainda não estão funcinais, mas irei resolver

---
+++ SiteBotoesFuncionais/script.js
@@ -0,0 +1,9 @@
+function name() {
+//     let num0 = prompt("Escolha um número !")
+//     if (num0 % 2 !== 0) {
+//         alert("O número é Impar")
+//     }else{ 
+//         alert("O número é Par")
+//  }
+    alert("Olá, seja bem-vindo ao meu site !")
+}
